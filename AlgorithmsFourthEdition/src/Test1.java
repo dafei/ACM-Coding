e8bee597fd1ce3123b73ed3feb4a842fabacc010
大飞 <zhangyf_zero@163.com>
2017-12-19 13:55:18 +0800

    AlgorithmsFourthEdition
    AlgorithmsFourthEdition codes

---
+++ AlgorithmsFourthEdition/src/Test1.java
@@ -0,0 +1,15 @@
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * Created by DaFei-PC on 2017-12-19.
+ */
+public class Test1 {
+
+    public static void main(String[] args)
+    {
+        StdOut.println(333);
+
+        return;
+    }
+
+}
